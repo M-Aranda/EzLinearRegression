5b918eeb43a6a2ae9ddd347fdc841e481ed58b55
Marcelo <cheloz_20@hotmail.com>
2020-09-23 19:34:54 -0300

    Proyecto puede sumar los numeros presentes

--- app/src/main/java/com/arandastock001/ezregre/ConfirmacionDeNumeros.java
+++ app/src/main/java/com/arandastock001/ezregre/ConfirmacionDeNumeros.java
@@ -26,26 +26,37 @@ public class ConfirmacionDeNumeros extends AppCompatActivity {
 
 
         ArrayList<Integer> listadoDeNumeros = new ArrayList<>();
+        //hay que hacerle un trim, sino reemplaza por -1's
+        stringDeCaraceresReconocidos = stringDeCaraceresReconocidos.trim();
 
         char[] partes = stringDeCaraceresReconocidos.toCharArray();
+
+
+
 
 
         Integer suma = 0;
        //String partes [] = stringDeCaraceresReconocidos.split("");
         for (int j = 0; j < partes.length  ; j++) {
 
-
             listadoDeNumeros.add(Character.getNumericValue(partes[j]));
 
-            suma = suma + Character.getNumericValue(partes[j]);
-
             System.out.println(partes[j]);
-
 
         }
 
 
 
+
+        for (int j = 0; j <listadoDeNumeros.size() ; j++) {
+            suma= suma+listadoDeNumeros.get(j);
+            System.out.println("El valor es "+listadoDeNumeros.get(j).toString());
+            System.out.println("La suma es "+suma.toString());
+        }
+
+        System.out.println("El tamaño de la lista es "+listadoDeNumeros.size());
+        System.out.println("El valor de la suma es " + suma.toString());
+        System.out.println("El listado de caracteres reconocidos es de "+partes.length);
 
 
         txtPrueba.setText(suma.toString());
