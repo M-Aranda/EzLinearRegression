37a5ae503dc4686c83030cbb3e80aacf299baec2
Marcelo <cheloz_20@hotmail.com>
2020-10-05 17:43:27 -0300

    creados los metodos para interactuar con los botones de creacion de archivo (aun no hacen nada)

--- app/src/main/java/com/arandastock001/ezregre/CreacionDeArchivo.java
+++ app/src/main/java/com/arandastock001/ezregre/CreacionDeArchivo.java
@@ -3,14 +3,54 @@ package com.arandastock001.ezregre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreacionDeArchivo extends AppCompatActivity {
+
+    private Button btnCrearExcel, btnCrearPDF, btnCrearTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creacion_de_archivo);
 
+        btnCrearExcel = (Button) findViewById(R.id.btnCrearExcel);
+        btnCrearPDF = (Button) findViewById(R.id.btnCrearPDF);
+        btnCrearTxt = (Button) findViewById(R.id.btnCrearTxt);
+
+
+
+
+        btnCrearExcel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        btnCrearPDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnCrearTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+
+
 
     }
+
+
 }
