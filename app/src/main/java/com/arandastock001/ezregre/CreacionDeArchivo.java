b7ba9e299458ac73441e73bc44ed39877a606ba3
Marcelo <cheloz_20@hotmail.com>
2020-10-07 20:49:08 -0300

    problemas para crear el archivo txt

--- app/src/main/java/com/arandastock001/ezregre/CreacionDeArchivo.java
+++ app/src/main/java/com/arandastock001/ezregre/CreacionDeArchivo.java
@@ -1,14 +1,21 @@
 package com.arandastock001.ezregre;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CreacionDeArchivo extends AppCompatActivity {
 
@@ -31,11 +38,6 @@ public class CreacionDeArchivo extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
-
-
-
-
             }
         });
 
@@ -51,8 +53,20 @@ public class CreacionDeArchivo extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+
+                Date momentoActual = Calendar.getInstance().getTime();
+                String nombreArchivo = "txt creado el "+momentoActual.toString();
+
+                String mensaje = "mensaje de prueba";
+
+
+
+
+
             }
-        });
+
+            }
+        );
 
 
 
