248ceae971e1b4639632a2c6a9fb518d4d731a93
chelo <cheloz_20@hotmail.com>
2021-04-11 05:23:07 -0400

    Ya es posible logearse (pero no salir), crear carpeta y subir archivos, pero falta pulir donde y como, tambien se solicitan todos los permisos de una

---
+++ app/src/main/java/com/arandastock001/EzLinearRegression/Gdrive/GoogleDriveFileHolder.java
@@ -0,0 +1,56 @@
+package com.arandastock001.EzLinearRegression.Gdrive;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+// GoogleDriveFileHolder class for storing the google drive file metadata
+public class GoogleDriveFileHolder implements Parcelable {
+
+    private String id;
+    private String name;
+
+    public GoogleDriveFileHolder(){}
+
+    private GoogleDriveFileHolder(Parcel in) {
+        id = in.readString();
+        name = in.readString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(name);
+    }
+
+    public static final Creator<GoogleDriveFileHolder> CREATOR
+            = new Creator<GoogleDriveFileHolder>() {
+        public com.arandastock001.EzLinearRegression.Gdrive.GoogleDriveFileHolder createFromParcel(Parcel in) {
+            return new com.arandastock001.EzLinearRegression.Gdrive.GoogleDriveFileHolder(in);
+        }
+
+        public com.arandastock001.EzLinearRegression.Gdrive.GoogleDriveFileHolder[] newArray(int size) {
+            return new com.arandastock001.EzLinearRegression.Gdrive.GoogleDriveFileHolder[size];
+        }
+    };
+}
---
+++ app/src/main/java/com/arandastock001/EzLinearRegression/Gdrive/ListActivity.java
@@ -0,0 +1,27 @@
+package com.arandastock001.EzLinearRegression.Gdrive;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.arandastock001.EzLinearRegression.R;
+
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+
+    ListView filesListView;
+    FilesAdapter filesAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        ArrayList<GoogleDriveFileHolder> folderFilesList = getIntent().getParcelableArrayListExtra("fileList");
+
+        this.filesListView = findViewById(R.id.id_file_list);
+        filesAdapter = new FilesAdapter(getApplicationContext(), folderFilesList);
+        filesListView.setAdapter(filesAdapter);
+    }
+}
